3d5837e4bfc6c6b8a0880141b8387dd38fbded11
lightningjim <lightningjim@users.noreply.github.com>
2015-11-26 10:23:49 -0600

    Crashing on CloudPebble, to test on actually Pebbl

--- src/KyleWxWatch2.c
+++ src/KyleWxWatch2.c
@@ -1,7 +1,29 @@
+
 #include <pebble.h>
 
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+static TextLayer *s_ampm_layer;
+static GFont s_time_font;
+static GFont s_ampm_font;
+
+static void update_time() {
+  // Get a tm structure
+  time_t temp = time(NULL); 
+  struct tm *tick_time = localtime(&temp);
+
+  // Write the current hours and minutes into a buffer
+  static char s_buffer[8];
+  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
+                                          "%H:%M" : "%I:%M", tick_time);
+
+  // Display this time on the TextLayer
+  text_layer_set_text(s_time_layer, s_buffer);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
+}
 
 static void main_window_load(Window *window) {
   // Get information about the Window
@@ -10,7 +32,9 @@ static void main_window_load(Window *window) {
 
   // Create the TextLayer with specific bounds
   s_time_layer = text_layer_create(
-      GRect(0, 0, bounds.size.w, 48));
+      GRect(0, 0, 114, 42));
+  s_time_layer = text_layer_create(
+      GRect(114, 0, 30, 24));
 
   // Improve the layout to be more like a watchface
   text_layer_set_background_color(s_time_layer, GColorClear);
@@ -18,17 +42,30 @@ static void main_window_load(Window *window) {
   text_layer_set_text(s_time_layer, "00:00");
   text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+  text_layer_set_background_color(s_time_layer, GColorBlack);
+  text_layer_set_text_color(s_time_layer, GColorWhite);
+  text_layer_set_text(s_time_layer, "MM");
+  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+  
+  // Create GFont
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_DEJAVU_SANS_BOLD_32));
+  s_ampm_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_DEJAVU_SANS_BOLD_10));
+
+  // Apply to TextLayer
+  text_layer_set_font(s_time_layer, s_time_font);  
+  text_layer_set_font(s_ampm_layer, s_ampm_font);  
 
   // Add it as a child layer to the Window's root layer
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
-
+  //layer_add_child(window_layer, text_layer_get_layer(s_ampm_layer));
 }
 
 static void main_window_unload(Window *window) {
   // Destroy TextLayer
   text_layer_destroy(s_time_layer);
-
 }
+
 
 static void init() {
   // Create main Window element and assign to pointer
@@ -42,12 +79,15 @@ static void init() {
 
   // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
+
+  // Make sure the time is displayed from the start
+  update_time();
+
+  // Register with TickTimerService
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 }
 
-
 static void deinit() {
-
-
   // Destroy Window
   window_destroy(s_main_window);
 }
